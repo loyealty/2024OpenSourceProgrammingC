7521812ae1ac676cd426c05f55b30de50111dcc2
jiyun7765 <jiyun2422@gmail.com>
2024-10-04 15:50:07 +0900

    v0.7 zero value

--- week06/main.go
+++ week06/main.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
-	var i int = 13
-	f := 12.9
-	fmt.Printf("value i: %d, value f: %f\n", i, f)
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f)
-	fmt.Println(reflect.Typeof(i), reflect.Typeof(f))
+	i := 13              //var i int = 13
+	var f float64 = 12.9 //f := 12.9
+	c1 := '2'            //50
+	c2 := '김'            //44608, unicode
+
+	fmt.Printf("value i : %d, value f : %f\n", i, f)
+	//fmt.Printf("%d * %f = %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
+	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
+	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // conversion
+	fmt.Println(c1, c2)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
