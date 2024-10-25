5905f4ec8102b01672b4d63f8fceb6587c3a56a0
jiyun7765 <jiyun2422@gmail.com>
2024-10-25 14:58:56 +0900

    v1.8

--- week09/main.go
+++ week09/main.go
@@ -16,25 +16,36 @@ func main() {
 	answer := rand.Intn(6) + 1
 	fmt.Println(answer)
 
-	fmt.Print("숫자 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	i, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+	var win bool = false
+	for guesses := 0; guesses < 3; guesses++ {
 
-	i = strings.TrimSpace(i)
-	guess, err := strconv.Atoi(i)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guess)
+		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
+		in := bufio.NewReader(os.Stdin)
+		i, err := in.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	if answer == guess {
-		fmt.Println("정답입니다.")
-	} else if answer > guess {
-		fmt.Println("입력한 수는 정답보다 작은 수 입니다. LOW")
+		i = strings.TrimSpace(i)
+		guess, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
+
+		if answer == guess {
+			fmt.Println("정답입니다.")
+			win = true
+			break
+		} else if answer > guess {
+			fmt.Println("입력한 수는 정답보다 작은 수 입니다. LOW")
+		} else {
+			fmt.Println("입력한 수는 정답보다 큰 수 입니다. HIGH")
+		}
+	}
+	if win {
+		fmt.Println("당신이 이겼습니다.")
 	} else {
-		fmt.Println("입력한 수는 정답보다 큰 수 입니다. HIGH")
+		fmt.Println("당신이 패배했습니다.")
 	}
 }
