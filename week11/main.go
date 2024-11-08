a763d62836df864bd84129df0a109128ba0021b3
jiyun7765 <jiyun2422@gmail.com>
2024-11-08 15:19:11 +0900

    v3.6 Prime number

--- week11/main.go
+++ week11/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-	"main.go/greeting"
+	"fmt"
+	"log"
+
+	"main.go/keyboard"
 )
 
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		j := 3
+		for j*j <= n {
+			if n%j == 0 {
+				return false
+			}
+			//fmt.Printf("%d ", j)
+			j += 2
+		}
+	}
+	return true
+}
+
 func main() {
-	greeting.EnglishGreetings("Inha")
+	fmt.Print("Input starting number : ")
+	n1, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Input end number : ")
+	n2, err := keyboard.GetInteger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for j := n1; j <= n2; j++ {
+		if isPrime(j) {
+			fmt.Printf("%d ", j)
+		}
+	}
+
 }
