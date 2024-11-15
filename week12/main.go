00efbf193a818642c39cf4bfcf503d0001cc8099
jiyun7765 <jiyun2422@gmail.com>
2024-11-15 14:48:31 +0900

    v3.8 Array

--- week12/main.go
+++ week12/main.go
@@ -8,9 +8,14 @@ import (
 func main() {
 	var scores [3]int
 	scores[1] = 90
-	fmt.Println(scores[1], scores[0]) //, scores[3])  // invalid argument: index 3 out of bounds
-	fmt.Printf("%#v\n", scores)
-	fmt.Println(scores)
+	// fmt.Println(scores[1], scores[0]) //, scores[3])  // invalid argument: index 3 out of bounds
+	// fmt.Printf("%#v\n", scores)
+	// fmt.Println(scores)
+
+	// for i := 0; i <= len(scores); i++ {
+	for i := 0; i <= len(scores); i++ {
+		fmt.Printf("%d ", scores[i])
+	}
 
 	// var dates [3]time.Time
 	// dates[1] = time.Unix(1947200001, 0)
@@ -33,7 +38,12 @@ func main() {
 		time.Unix(0, 0),
 		time.Unix(1, 0),
 		time.Unix(1947200001, 0)}
-	fmt.Println(dates[0], dates[1], dates[2])
-	fmt.Printf("%#v\n", dates)
-	fmt.Println(dates)
+	// fmt.Println(dates[0], dates[1], dates[2])
+	// fmt.Printf("%#v\n", dates)
+	// fmt.Println(dates)
+	// for i, date := range dates {
+	// 	fmt.Println(i, date)
+	for _, date := range dates {
+		fmt.Println(i, date)
+	}
 }
